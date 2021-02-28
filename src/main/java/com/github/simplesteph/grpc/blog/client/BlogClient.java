2ab96230e5e64d893c269ad1ac370de40b6d85be
yigitsahin <yigit.sahin@denk-it.com>
2021-02-28 22:41:18 +0300

    conflict  1 deleted from example branch

--- src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
+++ src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
@@ -9,8 +9,6 @@ public class BlogClient {
 
     public static void main(String[] args) {
         System.out.println("Hello I'm a gRPC client for Blog");
-        //CONFLICT1
-
         //CONFLICT2
         BlogClient main = new BlogClient();
         main.run();
