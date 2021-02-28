9f51cd971eb9eb9d633185206dde4dd6451381af
yigitsahin <yigit.sahin@denk-it.com>
2021-02-28 22:46:23 +0300

    Conflict 1-2-3 added example branch

--- src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
+++ src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
@@ -9,7 +9,9 @@ public class BlogClient {
 
     public static void main(String[] args) {
         System.out.println("Hello I'm a gRPC client for Blog");
+        //CONFLICT1
         //CONFLICT2
+        //CONFLICT3
         BlogClient main = new BlogClient();
         main.run();
         
