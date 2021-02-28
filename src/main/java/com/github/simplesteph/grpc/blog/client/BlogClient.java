12e8c109eedd778094c7c558b23fb70ee834c83f
yigitsahin <yigit.sahin@denk-it.com>
2021-02-28 22:59:17 +0300

    Last version

--- src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
+++ src/main/java/com/github/simplesteph/grpc/blog/client/BlogClient.java
@@ -8,16 +8,11 @@ public class BlogClient {
 
 
     public static void main(String[] args) {
+
         System.out.println("Hello I'm a gRPC client for Blog");
-        //CONFLICT1
-        //CONFLICT2
-        //CONFLICT3
-        //CONFLICT4
-        //CONFLICT5
-        //CONFLICT6
+
         BlogClient main = new BlogClient();
         main.run();
-        
     }
 
     private void run() {
